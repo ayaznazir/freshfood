4badce09a1fa8c1d11df3b6a1539c01e6bfc7ff7
ayaznazir <ayaznazir2002@gmail.com>
2022-12-05 15:14:07 +0500

    new commits

--- lib/views/auth_screen/splash_screen.dart
+++ lib/views/auth_screen/splash_screen.dart
@@ -4,6 +4,7 @@ import 'package:fresh_food/constants/app_constant.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import 'login_screen.dart';
+
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
 
@@ -12,45 +13,54 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
-
+  var a = "";
+  var b = "";
   @override
   void initState() {
-    Future.delayed(const Duration(seconds: 2) , (){
-      Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context) => const LoginScreen()), (route) => false);
+    Future.delayed(const Duration(seconds: 2), () {
+      Navigator.pushAndRemoveUntil(
+          context,
+          MaterialPageRoute(builder: (context) => const LoginScreen()),
+          (route) => false);
     });
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       backgroundColor: appColor,
       appBar: AppBar(
-          backgroundColor: appColor,
-          elevation: 0,
-          brightness: Brightness.dark,
+        backgroundColor: appColor,
+        elevation: 0,
+        brightness: Brightness.dark,
         systemOverlayStyle: const SystemUiOverlayStyle(
           systemNavigationBarColor: Colors.white, // navigation bar color
           statusBarColor: appColor, // status bar color
           statusBarIconBrightness: Brightness.dark, // status bar icons' color
-          systemNavigationBarIconBrightness: Brightness.dark, //navigation bar icons' color
+          systemNavigationBarIconBrightness:
+              Brightness.dark, //navigation bar icons' color
         ),
       ),
-      body: SafeArea(child:
-      Column(
+      body: SafeArea(
+          child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
-        children:  [
+        children: [
           Center(
-            child: Text("Fresh Food" , style: GoogleFonts.poppins(
-                color: Colors.white, fontSize: 30, fontWeight: FontWeight.bold
-            ), /* TextStyle(
+            child: Text(
+              "Fresh Food",
+              style: GoogleFonts.poppins(
+                  color: Colors.white,
+                  fontSize: 30,
+                  fontWeight: FontWeight
+                      .bold), /* TextStyle(
               color: Colors.white, fontSize: 30, fontWeight: FontWeight.bold
-            ),*/),
+            ),*/
+            ),
           )
         ],
-      )
-      ),
+      )),
     );
   }
 }
